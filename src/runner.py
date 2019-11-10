a90e92f4e8f9f3a494882809258e5b9a39120d72
flyb1z0n <flyb1z0n@gmail.com>
2019-11-11 00:16:11 +0200

    add delay

--- src/runner.py
+++ src/runner.py
@@ -1,5 +1,5 @@
 from thread_monitor import ThreadMonitor
 from board_monitor import BoardMonitor
 
-bm = BoardMonitor().start()
-tm = ThreadMonitor().start()
+bm = BoardMonitor().start(config.BOARD_MONITOR_DELAY_SECONDS)
+tm = ThreadMonitor().start(config.BOARD_MONITOR_DELAY_SECONDS)
