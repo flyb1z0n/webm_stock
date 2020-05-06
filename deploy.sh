bd10e50c6f9f7331aee99aa3eb3f1b028005ab8b
flyb1z0n <flyb1z0n@gmail.com>
2020-05-06 21:45:17 +0300

    fix deployment script v2

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 git pull
 
+cd _docker
 docker-compose -f ./_docker/prod-docker-compose.yml build
 docker-compose -f ./_docker/prod-docker-compose.yml stop
 docker-compose -f ./_docker/prod-docker-compose.yml up -d
