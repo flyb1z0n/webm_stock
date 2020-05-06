daa81e832065daeaa4725906e14135c289997bdc
flyb1z0n <flyb1z0n@gmail.com>
2020-05-06 21:45:46 +0300

    fix deployment script v3

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 git pull
 
 cd _docker
-docker-compose -f ./_docker/prod-docker-compose.yml build
-docker-compose -f ./_docker/prod-docker-compose.yml stop
-docker-compose -f ./_docker/prod-docker-compose.yml up -d
+docker-compose -f ./prod-docker-compose.yml build
+docker-compose -f ./prod-docker-compose.yml stop
+docker-compose -f ./prod-docker-compose.yml up -d
