b3de818027ba576e2d8f2b2ebe584973ae1a6867
flyb1z0n <flyb1z0n@gmail.com>
2020-08-17 16:45:02 +0300

    clean-up after vue js modal deletion

--- ui/src/main.js
+++ ui/src/main.js
@@ -1,13 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VModal from 'vue-js-modal'
 import InfiniteScroll from 'vue-infinite-scroll'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
-Vue.use(VModal)
 Vue.use(InfiniteScroll)
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
