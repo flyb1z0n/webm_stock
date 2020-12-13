2b30f78266c2dbf42941b71bd552227f1aa4565c
Konstantin Shavrukov <flyb1z0n@gmail.com>
2020-12-13 15:57:10 +0200

    Add Vuex store & retain volume lvl (#5)
    * first commit of store
    
    * Add Vuex Store;
    Save Volume level in browser;
    
    * Remove unnesessary code

--- ui/src/main.js
+++ ui/src/main.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App.vue'
 import InfiniteScroll from 'vue-infinite-scroll'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { store } from './store/store.js';
 
 Vue.config.productionTip = false
 Vue.use(InfiniteScroll)
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
+Vue.use(Vuex)
+
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  store: store
+})
